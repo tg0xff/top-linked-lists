417ab3550962d10cdc72e9880d679d83227d9925
tg0xff <tg0xff@proton.me>
2024-07-28 17:46:42 -0600

    Define LinkedList and append() method

--- linked-list.js
+++ linked-list.js
@@ -4,3 +4,21 @@ class Node {
     this.nextNode = null;
   }
 }
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+  append(value) {
+    const node = new Node();
+    node.value = value;
+    if (this.head === null) {
+      this.head = node;
+    }
+    this.tail.nextNode = node;
+    this.tail = node;
+    this.size++;
+  }
+}
